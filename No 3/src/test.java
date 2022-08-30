2466fc090c7127799eb91b17388cdcb13e7bf16e
khaledyurino <71624020+khaledyurino@users.noreply.github.com>
2022-08-30 20:59:50 +0700

    Add files via upload

---
+++ No 3/src/test.java
@@ -0,0 +1,23 @@
+public class test{
+
+    public static void main(String args[]) {
+        int[] numbers = { 2, 4, 3, 5, 7, 8, 9 };
+        printPairs(numbers, 7);
+
+    }
+
+    public static void printPairs(int[] array, int sum) {
+
+        for (int i = 0; i < array.length; i++) {
+            int first = array[i];
+            for (int j = i + 1; j < array.length; j++) {
+                int second = array[j];
+
+                if ((first + second) == sum) {
+                    System.out.printf("(%d, %d) %n", first, second);
+                }
+            }
+
+        }
+    }
+}
